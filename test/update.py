fc1a83530ec0285ac00971bcc003281b9fe0f083
codingPro01 <68053011+codingPro01@users.noreply.github.com>
2021-02-28 12:00:15 +0900

    Update update.py

--- test/update.py
+++ test/update.py
@@ -7,5 +7,13 @@ if nowversion != checkver:
     print("Update available. ")
     print(f"Now version: {nowversion}")
     print(f"New version: {checkver}")
+    confirm_new_version_download = input(f"Download {checkver} program? (y/n): ")
+    if confirm_new_version_download == "y":
+        newappdnpage = str(urllib.request.urlopen("https://raw.githubusercontent.com/codingPro01/PyUpdateChacker_Test/main/test/update.py").read()).replace("b'", "")
+        newappdn = newappdnpage.replace("\\n", "\n")
+        newappdn = newappdn[:-1]
+        newappins = open("updatecheck_1.py", "w+")
+        newappins.write(newappdn)
 if nowversion == checkver:
     print("Program up to date. ")
+ 
